ce1f3fa60dcda7216a8a05151c4331bd71017eaf
jessica kilby <jessicakilby@gmail.com>
2016-08-21 11:01:35 -0500

    Finally completely done!

--- quiz.js
+++ quiz.js
@@ -7,20 +7,34 @@ var tree = {
 var submitButton = document.getElementById("submit");
 var clearButton = document.getElementById("clear");
 var output = document.getElementById("treeDOM");
-// var lines = [];
+
 
 function growYourTree () {
 	var howTall = tree.height.value;
 	var whatChar = tree.character.value;
-	// var counter = 1;
+	
+	if (howTall === ""){
+		alert("You didn't put in a height");
+	} else if (whatChar === ""){
+		alert("Enter a character");
+	} else {
+		console.log("Here is your tree");
+	}
+
 	for (var i = 0; i < howTall; i++) {
+
 		var math = i*2+1;
 		var width = whatChar.repeat(math);
+
+
 		var spaceMath = ((howTall*2-1)-math)/2;
 		var widthSpace = " ".repeat(spaceMath);
+		
 		console.log(widthSpace + width);
 	}
 }
+
+
 
 function enterKeyPressed(keypress){
  if (keypress.which === 13) {
@@ -38,10 +52,6 @@ submitButton.addEventListener("click", growYourTree);
 document.addEventListener("keypress", enterKeyPressed)
 clearButton.addEventListener("click", clear);
 
-
-// Create a tree function that should build a pine tree out of a character in the Chrome dev tools console. It accepts a single object as an argument. The object should have two key/value pairs.
-
-// Once the user enters in a number, and a character, the user can either then just press the enter key (as long as the cursor is in one of the input fields), or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
 
 // If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value.
 
