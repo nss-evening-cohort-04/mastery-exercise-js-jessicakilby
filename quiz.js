c14ef435fb681930231af5c4f0fb0bfe36ed1327
jessica kilby <jessicakilby@gmail.com>
2016-08-22 21:17:36 -0500

    Changes to final submission

--- quiz.js
+++ quiz.js
@@ -1,44 +1,53 @@
 
+
 var tree = {
-	height: document.getElementById("height"),
-	character: document.getElementById("character")
+	height: "",
+	character: ""
 };
 
 var submitButton = document.getElementById("submit");
 var clearButton = document.getElementById("clear");
 var output = document.getElementById("treeDOM");
+var height = document.getElementById("height");
+var character =  document.getElementById("character");
 
+function validation(){
 
-function growYourTree () {
-	var howTall = tree.height.value;
-	var whatChar = tree.character.value;
+var tree = {
+	height: height.value,
+	character: character.value
+};
 	
-	if (howTall === ""){
+	if (tree.height === ""){
 		alert("You didn't put in a height");
-	} else if (whatChar === ""){
+	} else if (tree.character === ""){
 		alert("Enter a character");
 	} else {
 		console.log("Here is your tree");
+		growYourTree(tree);
 	}
 
-	for (var i = 0; i < howTall; i++) {
+}
+
+
+function growYourTree (myTree) {
+	
+	for (var i = 0; i < myTree.height; i++) {
 
 		var math = i*2+1;
-		var width = whatChar.repeat(math);
+		var width = myTree.character.repeat(math);
 
 
-		var spaceMath = ((howTall*2-1)-math)/2;
+		var spaceMath = ((myTree.height*2-1)-math)/2;
 		var widthSpace = " ".repeat(spaceMath);
 		
 		console.log(widthSpace + width);
 	}
 }
 
-
-
 function enterKeyPressed(keypress){
  if (keypress.which === 13) {
-   growYourTree();
+   validation();
  }
 }
 
@@ -48,11 +57,11 @@ function clear() {
 	document.getElementById("character").value = "";
 }
 
-submitButton.addEventListener("click", growYourTree);
+submitButton.addEventListener("click", validation);
 document.addEventListener("keypress", enterKeyPressed)
 clearButton.addEventListener("click", clear);
 
 
-// If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value.
+
 
 
